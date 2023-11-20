4487e8945fa4ddaac6e811d2c2572538e71b469d
carlosmatac <148758556+carlosmatac@users.noreply.github.com>
2023-11-20 21:37:35 +0100

    Add files via upload

---
+++ Caclculator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caclculator;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Caclculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Screen screen = new Screen();
+        screen.setBounds(400, 200, 420, 350);
+        screen.setVisible(true);
+        
+    }
+    
+}
